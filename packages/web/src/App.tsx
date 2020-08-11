259d10462d5d4099bd84a3cbf42d1c04b6ce1f75
Bryan Lackey <lackeyjb1@gmail.com>
2020-08-11 19:49:16 -0400

    First commit

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@chakra-ui/core';
+
+function App() {
+  return (
+    <Box bg="hotpink" w={1 / 2}>
+      Hello world
+    </Box>
+  );
+}
+
+export default App;
